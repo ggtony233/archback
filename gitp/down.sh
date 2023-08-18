8c8f89296a77d7237aaccb8c413911835fa3c793
ggtony <1923113775@qq.com>
2023-08-18 21:46:50 +0800

    init&update

---
+++ gitp/down.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ndomain=`echo $2|cut -d '/' -f 3`;
+mirrorurl="ghproxy.com";
+case $ndomain in 
+	"github.com"|"raw.githubusercontent.com")
+		url="https://"$mirrorurl/$2;
+		echo "$mirrorurl"
+		;;
+	*)
+		url=$2;
+		;;
+esac
+echo  -e "download from $url\n"
+/usr/bin/axel -n 8 -a -o $1 $url
