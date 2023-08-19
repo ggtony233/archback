e88dde46fe7bfc96413e02d43c4fdd970b541051
ggtony <1923113775@qq.com>
2023-08-19 11:34:03 +0800

    修改：     thinkpade480_archinstall/install.sh

--- thinkpade480_archinstall/install.sh
+++ thinkpade480_archinstall/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rootpasswd=""
+passwd=""
 ln -sf  /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock --systohc
 sed -i 's/\#en_SG.UTF-8 UTF-8/en_SG.UTF-8 UTF-8/g' /etc/locale.gen
@@ -9,7 +11,7 @@ wait
 echo 'LANG=en_SG.UTF-8' >> /etc/locale.conf
 echo 'thinkpad-e480' >>/etc/hostname
 echo -e '127.0.0.1 localhost\n::1 localhost\n127.0.1.1 thinkpad-e480.localdomain thinkpad-e480'
-echo -e 'xutao911\nxutao911'|(passwd root)
+echo -e '$ROOTPASSWD\n$ROOTPASSWD'|(passwd root)
 wait
 echo -e '[archlinuxcn]\nServer = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' >> /etc/pacman.conf
 pacman -Syu
@@ -21,7 +23,7 @@ sed -i 's/MODULES=()/MODULES=(btrfs xfs)/g' /etc/mkinitcpio.conf
 mkinitcpio -P
 wait
 useradd -m -G wheel -s /bin/zsh ggtony
-echo -e 'xutao911\nxutao911'|(passwd ggtony)
+echo -e '$PASSWD\n$PASSWD'|(passwd ggtony)
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ArchLinux
 wait
 grub-mkconfig -o /boot/grub/grub.cfg
